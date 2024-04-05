2d8f549b860c2ae2931a8b48533d1d13422a3c09
Tania Pan <tania.pan1511@gmail.com>
2024-04-06 00:20:45 +1300

    added error messages to makeBooking method if conditions are not met and improved way to check if date is in the past

--- src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
+++ src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
@@ -108,10 +108,14 @@ public class VenueHireSystem {
     /* checking booking conditions are met */
 
     // checking system date is set
-    if (systemDate == null) {}
+    if (systemDate == null) {
+      MessageCli.BOOKING_NOT_MADE_DATE_NOT_SET.printMessage();
+      return;
+    }
 
     // checking there is at least one venue in the system
     if (venueList.isEmpty()) {
+      MessageCli.BOOKING_NOT_MADE_NO_VENUES.printMessage();
       return;
     }
 
@@ -120,6 +124,7 @@ public class VenueHireSystem {
       if (venue.getVenueCode().equals(options[0])) {
         break;
       } else {
+        MessageCli.BOOKING_NOT_MADE_VENUE_NOT_FOUND.printMessage(options[0]);
         return;
       }
     }
@@ -127,25 +132,16 @@ public class VenueHireSystem {
     // checking venue is available on the date
     for (Venue venue : venueList) {
       if (venue.getVenueDate().equals(options[1])) {
+        MessageCli.BOOKING_NOT_MADE_VENUE_ALREADY_BOOKED.printMessage(
+            venue.getVenueName(), options[1]);
         return;
       }
     }
 
     // checking the date is not in the past
-
-    String[] parsedInputDate = options[1].split("/");
-    String[] parsedSystemDate = systemDate.split("/");
-
-    if (Integer.parseInt(parsedInputDate[2]) < Integer.parseInt(parsedSystemDate[2])) {
+    if (isDateInPast(options[1], systemDate) == false) {
+      MessageCli.BOOKING_NOT_MADE_PAST_DATE.printMessage(options[1], systemDate);
       return;
-    } else if (Integer.parseInt(parsedInputDate[2]) == Integer.parseInt(parsedSystemDate[2])) {
-      if (Integer.parseInt(parsedInputDate[1]) < Integer.parseInt(parsedSystemDate[1])) {
-        return;
-      } else if (Integer.parseInt(parsedInputDate[1]) == Integer.parseInt(parsedSystemDate[1])) {
-        if (Integer.parseInt(parsedInputDate[0]) < Integer.parseInt(parsedSystemDate[0])) {
-          return;
-        }
-      }
     }
   }
 
@@ -179,11 +175,24 @@ public class VenueHireSystem {
     }
   }
 
-  // parsing an entered date
-  public void parseDate(String date) {
-    String[] dateParts = date.split("/");
-    String day = dateParts[0];
-    String month = dateParts[1];
-    String year = dateParts[2];
+  // checking if date is in the past
+  public boolean isDateInPast(String inputDate, String systemDate) {
+
+    String[] parsedInputDate = inputDate.split("/");
+    String[] parsedSystemDate = systemDate.split("/");
+
+    if (Integer.parseInt(parsedInputDate[2]) < Integer.parseInt(parsedSystemDate[2])) {
+      return false;
+    } else if (Integer.parseInt(parsedInputDate[2]) == Integer.parseInt(parsedSystemDate[2])) {
+      if (Integer.parseInt(parsedInputDate[1]) < Integer.parseInt(parsedSystemDate[1])) {
+        return false;
+      } else if (Integer.parseInt(parsedInputDate[1]) == Integer.parseInt(parsedSystemDate[1])) {
+        if (Integer.parseInt(parsedInputDate[0]) < Integer.parseInt(parsedSystemDate[0])) {
+          return false;
+        }
+      }
+    }
+
+    return true; // else date is not in the past
   }
 }
